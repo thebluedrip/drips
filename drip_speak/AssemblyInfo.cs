68611ce34238cf15d42cb7a3236342aa99c213bd
lookatthefeatures <53217027+thebluedrip@users.noreply.github.com>
2022-12-27 00:17:59 +0000

    moved files from "drips" to here
    the tutorial of changing the folder name was outdated and i had no choice but to just reupload everything because it was the only easy option. WHY CAN'T GITHUB GUI HAVE A RENAME SECTION FOR FOLDERS???

---
+++ drip_speak/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Encrypt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Encrypt")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ drip_speak/Properties/Resources.cs
@@ -0,0 +1,141 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Encrypt.Properties.Resources
+// Assembly: Encrypt, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: B0155084-1977-4D4E-A8CB-70DDE86B02BC
+// Assembly location: C:\Users\Klonoa\Documents\Shared\drip_speak.exe
+
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace Encrypt.Properties
+{
+  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+  [DebuggerNonUserCode]
+  [CompilerGenerated]
+  internal class Resources
+  {
+    private static ResourceManager resourceMan;
+    private static CultureInfo resourceCulture;
+
+    internal Resources()
+    {
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+      get
+      {
+        if (Encrypt.Properties.Resources.resourceMan == null)
+          Encrypt.Properties.Resources.resourceMan = new ResourceManager("Encrypt.Properties.Resources", typeof (Encrypt.Properties.Resources).Assembly);
+        return Encrypt.Properties.Resources.resourceMan;
+      }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static CultureInfo Culture
+    {
+      get => Encrypt.Properties.Resources.resourceCulture;
+      set => Encrypt.Properties.Resources.resourceCulture = value;
+    }
+
+    internal static Bitmap loading00000 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00000), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00001 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00001), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00002 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00002), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00003 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00003), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00004 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00004), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00005 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00005), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00006 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00006), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00007 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00007), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00008 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00008), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00009 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00009), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00010 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00010), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00011 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00011), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00012 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00012), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00013 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00013), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00014 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00014), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00015 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00015), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00016 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00016), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00017 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00017), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00018 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00018), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00019 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00019), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00020 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00020), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00021 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00021), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00022 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00022), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00023 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00023), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00024 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00024), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00025 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00025), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00026 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00026), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00027 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00027), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00028 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00028), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00029 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00029), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00030 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00030), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00031 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00031), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00032 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00032), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00033 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00033), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00034 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00034), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00035 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00035), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00036 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00036), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00037 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00037), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00038 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00038), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00039 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00039), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00040 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00040), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00041 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00041), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00042 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00042), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00043 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00043), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00044 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00044), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00045 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00045), Encrypt.Properties.Resources.resourceCulture);
+
+    internal static Bitmap loading00046 => (Bitmap) Encrypt.Properties.Resources.ResourceManager.GetObject(nameof (loading00046), Encrypt.Properties.Resources.resourceCulture);
+  }
+}
